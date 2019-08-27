d0f48b8d3008ea5211be0d0259eadcfe55ecf277
wwwtalal <github@ewebmatrix.com>
2019-08-27 11:52:45 +0300

    Add files via upload

---
+++ precache-manifest.e3cc45c048ac709945f484674bbec984.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaef36413bafce54fd5c2203df7aabd3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "510877dbea50b3c6c12b",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "394996b0e427db498ff5",
+    "url": "/static/js/2.181b901d.chunk.js"
+  },
+  {
+    "revision": "510877dbea50b3c6c12b",
+    "url": "/static/js/main.eb06cef1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
